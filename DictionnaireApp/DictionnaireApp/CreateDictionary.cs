0606ea070ad8ba0e34fa1ec01fd6d57890f4ce66
Elizabeth0014 <115196592+Elizabeth0014@users.noreply.github.com>
2023-03-08 16:20:04 -0500

    MakeAList

---
+++ DictionnaireApp/DictionnaireApp/CreateDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionnaireApp
+{
+    internal class CreateDictionary
+    {
+        public List<string> MakeAList(List<string> ListEntree, int min, int max)
+        {
+            max = max - 1;
+            List<string> listTemp = new List<string>();
+            listTemp.AddRange(ListEntree);
+
+            List<string> listInitial = new List<string>();
+            listInitial.AddRange(ListEntree);
+            int counter = 0;
+            int skipToIndex = 0;
+
+            // pour avoir des éléments de longeur max
+            for (int i = 0; i < max; i++)
+            {
+                //pour traverser la liste d'entrée
+                // on saute les éléments qu'un a déjà fait avec skipToIndex
+                for (int j = 0 + skipToIndex; j < listTemp.Count; j++)
+                {
+                    //créer un nouvel élément qui contient l'élément de la liste actuelle + l'élément x de la liste initiale
+                    // faire un nouvel élément pour chaque élément de la liste initiale
+                    foreach (var value in listInitial)
+                    {
+                        string nouveau = listTemp[j] + value;
+                        ListEntree.Add(nouveau);
+                    }
+                    counter++;
+
+                    //// print pour voir l'évolution de la liste
+                    //foreach (var value in ListEntree)
+                    //    Console.Write(value +", ");
+                    //Console.WriteLine();
+                }
+                skipToIndex = counter;
+                counter = 0;
+
+                //ajouter les nouveaux éléments créés
+                // utiliser Clear() pour pas se répéter
+                listTemp.Clear();
+                listTemp.AddRange(ListEntree);
+            }
+            // traverser la liste qu'on a obtenu
+            // si la longueur de la chaine est plus petite que le minimum, on l'enlève
+            foreach (var result in listTemp)
+            {
+                if (result.Length < min)
+                {
+                    ListEntree.Remove(result);
+                }
+            }
+            return ListEntree;
+        }
+    }
+}
--- DictionnaireApp/DictionnaireApp/UI_GenerateurDeDictionnaire.Designer.cs
+++ DictionnaireApp/DictionnaireApp/UI_GenerateurDeDictionnaire.Designer.cs
@@ -34,30 +34,33 @@
             this.tb_path = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.clb_choix_char = new System.Windows.Forms.CheckedListBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numUD_max = new System.Windows.Forms.NumericUpDown();
             this.numUD_min = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_max)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numUD_min)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_creer_dict
             // 
             this.btn_creer_dict.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_creer_dict.Location = new System.Drawing.Point(16, 406);
+            this.btn_creer_dict.Location = new System.Drawing.Point(21, 500);
+            this.btn_creer_dict.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_creer_dict.Name = "btn_creer_dict";
-            this.btn_creer_dict.Size = new System.Drawing.Size(370, 23);
+            this.btn_creer_dict.Size = new System.Drawing.Size(493, 28);
             this.btn_creer_dict.TabIndex = 19;
             this.btn_creer_dict.Text = "Creer le dictionnaire";
             this.btn_creer_dict.UseVisualStyleBackColor = true;
+            this.btn_creer_dict.Click += new System.EventHandler(this.btn_creer_dict_Click);
             // 
             // btn_choisir_file
             // 
             this.btn_choisir_file.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_choisir_file.Location = new System.Drawing.Point(332, 360);
+            this.btn_choisir_file.Location = new System.Drawing.Point(443, 443);
+            this.btn_choisir_file.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_choisir_file.Name = "btn_choisir_file";
-            this.btn_choisir_file.Size = new System.Drawing.Size(54, 23);
+            this.btn_choisir_file.Size = new System.Drawing.Size(72, 28);
             this.btn_choisir_file.TabIndex = 18;
             this.btn_choisir_file.Text = "Fichier";
             this.btn_choisir_file.UseVisualStyleBackColor = true;
@@ -67,26 +70,29 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 337);
+            this.label4.Location = new System.Drawing.Point(16, 415);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(258, 22);
+            this.label4.Size = new System.Drawing.Size(327, 26);
             this.label4.TabIndex = 17;
             this.label4.Text = "Choix du fichier de destination";
             // 
             // tb_path
             // 
-            this.tb_path.Location = new System.Drawing.Point(16, 362);
+            this.tb_path.Location = new System.Drawing.Point(21, 446);
+            this.tb_path.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb_path.Name = "tb_path";
-            this.tb_path.Size = new System.Drawing.Size(310, 20);
+            this.tb_path.Size = new System.Drawing.Size(412, 22);
             this.tb_path.TabIndex = 16;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Location = new System.Drawing.Point(16, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(241, 22);
+            this.label3.Size = new System.Drawing.Size(301, 26);
             this.label3.TabIndex = 15;
             this.label3.Text = "Choix des caractères permis";
             // 
@@ -104,30 +110,32 @@
             "?",
             "&",
             "*"});
-            this.clb_choix_char.Location = new System.Drawing.Point(16, 123);
+            this.clb_choix_char.Location = new System.Drawing.Point(21, 151);
+            this.clb_choix_char.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clb_choix_char.Name = "clb_choix_char";
-            this.clb_choix_char.Size = new System.Drawing.Size(370, 193);
+            this.clb_choix_char.Size = new System.Drawing.Size(492, 229);
             this.clb_choix_char.TabIndex = 14;
             // 
-            // numericUpDown1
+            // numUD_max
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(342, 53);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.numUD_max.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numUD_max.Location = new System.Drawing.Point(456, 65);
+            this.numUD_max.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numUD_max.Maximum = new decimal(new int[] {
             50,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.numUD_max.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(44, 29);
-            this.numericUpDown1.TabIndex = 13;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.numUD_max.Name = "numUD_max";
+            this.numUD_max.Size = new System.Drawing.Size(59, 35);
+            this.numUD_max.TabIndex = 13;
+            this.numUD_max.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numUD_max.Value = new decimal(new int[] {
             1,
             0,
             0,
@@ -136,7 +144,8 @@
             // numUD_min
             // 
             this.numUD_min.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numUD_min.Location = new System.Drawing.Point(342, 8);
+            this.numUD_min.Location = new System.Drawing.Point(456, 10);
+            this.numUD_min.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numUD_min.Maximum = new decimal(new int[] {
             50,
             0,
@@ -148,7 +157,7 @@
             0,
             0});
             this.numUD_min.Name = "numUD_min";
-            this.numUD_min.Size = new System.Drawing.Size(44, 29);
+            this.numUD_min.Size = new System.Drawing.Size(59, 35);
             this.numUD_min.TabIndex = 12;
             this.numUD_min.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.numUD_min.Value = new decimal(new int[] {
@@ -161,9 +170,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(16, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(328, 22);
+            this.label2.Size = new System.Drawing.Size(411, 26);
             this.label2.TabIndex = 11;
             this.label2.Text = "Longueur maximum des mots de passe:";
             // 
@@ -171,30 +181,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 22);
+            this.label1.Size = new System.Drawing.Size(407, 26);
             this.label1.TabIndex = 10;
             this.label1.Text = "Longueur minimum des mots de passe:";
             // 
             // UI_GenerateurDeDictionnaire
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 450);
+            this.ClientSize = new System.Drawing.Size(540, 554);
             this.Controls.Add(this.btn_creer_dict);
             this.Controls.Add(this.btn_choisir_file);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.tb_path);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.clb_choix_char);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numUD_max);
             this.Controls.Add(this.numUD_min);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UI_GenerateurDeDictionnaire";
             this.Text = "UI_GenerateurDeDictionnaire";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_max)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numUD_min)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -209,7 +221,7 @@
         private System.Windows.Forms.TextBox tb_path;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckedListBox clb_choix_char;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numUD_max;
         private System.Windows.Forms.NumericUpDown numUD_min;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
